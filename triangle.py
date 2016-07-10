d5d59d2cb57f41704e1c5a25f0337b8dcefaba5d
Gabriel Dube <gdube.475@gmail.com>
2016-07-10 15:46:20 -0400

    vulkan enums and instance creation

--- triangle.py
+++ triangle.py
@@ -9,7 +9,8 @@
     @author: Gabriel Dubé
 """
 
-import platform, asyncio
+import platform, asyncio, vk
+from ctypes import cast, c_char_p, pointer, POINTER, byref
 
 system_name = platform.system()
 if system_name == 'Windows':
@@ -21,9 +22,42 @@ else:
 
 class Application(object):
 
+    def create_instance(self):
+        """
+            Setup the vulkan instance
+        """
+        app_info = vk.ApplicationInfo(
+            s_type=vk.STRUCTURE_TYPE_APPLICATION_INFO, next=vk.NULL,
+            application_name=b'PythonText', application_version=0,
+            engine_name=b'test', engine_version=0, api_version=((1<<22) | (0<<12) | (0))
+        )
+
+        if system_name == 'Windows':
+            extensions = (b'VK_KHR_surface', b'VK_KHR_win32_surface')
+        else:
+            extensions = (b'VK_KHR_surface', b'VK_KHR_xcb_surface')
+
+        extensions = [c_char_p(x) for x in extensions]
+        _extensions = cast((c_char_p*len(extensions))(*extensions), POINTER(c_char_p))
+
+        create_info = vk.InstanceCreateInfo(
+            s_type=vk.STRUCTURE_TYPE_INSTANCE_CREATE_INFO, next=vk.NULL, flags=0,
+            application_info=pointer(app_info), enabled_layer_count=0,
+            enabled_layer_names=vk.NULL_LAYERS, enabled_extension_count=len(extensions),
+            enabled_extension_names=_extensions
+        )
+
+        instance = vk.Instance(0)
+        result = vk.CreateInstance(byref(create_info), vk.NULL, byref(instance))
+        if result == vk.SUCCESS:
+            print(instance)
+            return instance
+        else:
+            raise RuntimeError('Instance creation failed. Error code: {}'.format(result))
+
     def __init__(self):
-        
         self.window = Window()
+        self.instance = self.create_instance()
 
 def main():
     # I never execute my code in the global scope of the project to make sure
